b42aeecadddf6c520478e24a6bcb534918348936
sanjoycode97 <sanjoysarkar.ece.55@gmail.com>
2025-04-19 01:17:28 +0530

    here changes added Ml model integrated

---
+++ README.md
@@ -0,0 +1,9 @@
+We have 4 https method:
+
+get
+post
+put 
+delete
+
+
+here we need to do the POST operation to submit the form with details
--- flask/main.py
+++ flask/main.py
@@ -1,9 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import joblib
 
 
 #initialize app
 
 app=Flask(__name__)
+
+model = joblib.load('../saving_model/saved_logitic_model.pkl')
 
 #create the route using the decorator
 @app.route("/")
@@ -25,6 +28,40 @@ def gallery():
 @app.route("/courses")
 def courses():
     return render_template("courses.html")
+
+@app.route("/pred")
+def predictions():
+    return render_template("data_input.html")
+
+@app.route("/action",methods=["POST"])
+def action():
+    #return "form has been submitted successfully"
+    data=[]
+    for val in request.form.values():
+        data.append(val)
+    print(data)
+
+    return f'data successfully added'
+
+@app.route("/ML_pred")
+def ML_predictions():
+    return render_template("ML_form.html")
+
+@app.route("/pred_result",methods=["POST"])
+def pred_result():
+    #return "form has been submitted successfully"
+    data=[]
+    for val in request.form.values():
+        data.append(float(val))
+    result=model.predict([data])
+
+    print(result)
+
+    if result[0]==1:
+        return 'pateint is diabetic'
+    return f'pateient not diabetic'
+
+
 
 
 #run the app
